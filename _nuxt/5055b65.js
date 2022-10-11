5f39d367a4506a65adc126b7d2d2be4d31ba6b83
shuukhratbegmatov <shuukhratbegmatov@gmail.com>
2022-10-11 16:21:14 +0500

    fix

--- _nuxt/5055b65.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{353:function(n,t,e){"use strict";e.r(t);var o=e(69),component=Object(o.a)({},(function(){return(0,this._self._c)("footer")}),[],!1,null,null,null);t.default=component.exports}}]);
