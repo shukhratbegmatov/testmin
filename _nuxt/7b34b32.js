a62121a7a5ea345c2774d11453180f2ca381faf3
shuukhratbegmatov <shuukhratbegmatov@gmail.com>
2022-10-31 18:06:32 +0500

    fix

---
+++ _nuxt/7b34b32.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{417:function(n,w,o){"use strict";o.r(w),w.default={}}}]);
---
+++ _nuxt/eb207cd.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{419:function(n,w,o){"use strict";o.r(w),w.default={}}}]);
