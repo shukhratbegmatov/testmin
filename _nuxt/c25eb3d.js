adfaf8358d17e2b8bb7291505e10ed9fb8e92f02
shuukhratbegmatov <shuukhratbegmatov@gmail.com>
2022-11-01 16:43:12 +0500

    fix

---
+++ _nuxt/c25eb3d.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[32],{443:function(n,l,t){"use strict";t.r(l);var e=t(75),component=Object(e.a)({},(function(){return(0,this._self._c)("div",[this._v("\n  Hello\n")])}),[],!1,null,null,null);l.default=component.exports}}]);
--- _nuxt/f8443f8.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[29],{439:function(n,l,t){"use strict";t.r(l);var e=t(75),component=Object(e.a)({},(function(){return(0,this._self._c)("div",[this._v("\n  Hello\n")])}),[],!1,null,null,null);l.default=component.exports}}]);
